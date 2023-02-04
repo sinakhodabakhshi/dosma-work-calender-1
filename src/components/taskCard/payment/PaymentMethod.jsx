9f9110a815ca247902ca61f79234b893bf9f9e14
sina <skhodabakhshi77@gmail.com>
2023-02-04 08:34:28 +0100

    last check

--- src/components/taskCard/payment/PaymentMethod.jsx
+++ src/components/taskCard/payment/PaymentMethod.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-
-export default function PaymentMethod({paymentMethod}) {
+export default function PaymentMethod({ paymentMethod }) {
   return (
-    <p className="text-end mt-5 font-semibold text-red-400 ">
-      {paymentMethod}
-    </p>
+    <p className="text-end mt-5 font-semibold text-red-400 ">{paymentMethod}</p>
   );
 }
